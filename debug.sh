ae78bf1bbf0b17e3305086b4361c8157992008d5
Florian Utz <florian.utz@github.com>
2018-01-04 12:35:45 +0100

    .

--- debug.sh
+++ debug.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 ps auxwww
+mount
+find /etc/systemd/system \
+         /lib/systemd/system \
+         -path '*.wants/*' \
+         -not -name '*journald*' \
+         -not -name '*systemd-tmpfiles*' \
+         -not -name '*systemd-user-sessions*' \
+         -exec rm \{} \;
+systemctl set-default multi-user.target
+
 apt update
 apt install chrony -y
 systemctl status chrony
